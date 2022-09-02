a5cb05083a584232e2d8e0fafe26f85e2cc0e5af
Camilo Santander <camilo.santander1991@gmail.com>
2022-09-02 00:05:35 -0400

    Curso Flutter 220

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -15,12 +15,18 @@ class ProductCard extends StatelessWidget {
         child: Stack(
           alignment: Alignment.bottomLeft,
           children: [
-            _BackgroundImage(),
-            _ProductDetails(),
+            const _BackgroundImage(),
+            const _ProductDetails(),
             Positioned(
               top: 0,
               right: 0,
               child: _PriceTag(),
+            ),
+            //TODO: Mostrar de manera condicional
+            Positioned(
+              top: 0,
+              left: 0,
+              child: _NotAvailable(),
             ),
           ],
         ),
@@ -42,11 +48,60 @@ class ProductCard extends StatelessWidget {
   }
 }
 
+class _NotAvailable extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Colors.yellow[700],
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(25),
+          bottomRight: Radius.circular(25),
+        ),
+      ),
+      child: const FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'No Disponible',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class _PriceTag extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Text('\$103.99'),
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(25),
+          bottomLeft: Radius.circular(25),
+        ),
+      ),
+      height: 70,
+      width: 100,
+      child: const FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            '\$103.99',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -59,9 +114,9 @@ class _ProductDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(right: 50),
+      padding: const EdgeInsets.only(right: 50),
       child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         width: double.infinity,
         height: 70,
         decoration: _buildBoxDecoration(),
